d86983dfb668c1c41cf5444f6674debb9e95b66e
congqin yan <1727053412@qq.com>
2022-04-02 21:06:25 +0100

    update to navigation

--- app/src/main/java/com/journey/activity/LeaderPeerGroupActivity.java
+++ app/src/main/java/com/journey/activity/LeaderPeerGroupActivity.java
@@ -71,7 +71,8 @@ public class LeaderPeerGroupActivity extends AppCompatActivity {
     CountDownTimer countDownTimer;
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
     final private static String MATCHED_PEERS = "MATCHED_PEERS";
-    LoadingDialog loadingDialog = new LoadingDialog(this,  8000, 2000, "");
+    LoadingDialog loadingDialog = new LoadingDialog(this, 8000, 2000, "");
+    List<Peer> returnPeers;
     Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://192.168.0.81:8080/")
             .addConverterFactory(GsonConverterFactory.create())
@@ -96,6 +97,7 @@ public class LeaderPeerGroupActivity extends AppCompatActivity {
         try {
             loadingDialog.startLoadingDialog();
             sendMultiRequests(8000, 2000);
+            sendPeersToNavigation(returnPeers);
             cancelJourney();
 
         } catch (Exception e) {
@@ -108,8 +110,9 @@ public class LeaderPeerGroupActivity extends AppCompatActivity {
         new CountDownTimer(totalTime, interval) {
             @Override
             public void onTick(long millisUntilFinished) {
-                createPost(retrofit);
+                createLeaderPost(retrofit);
             }
+
             @Override
             public void onFinish() {
 
@@ -117,13 +120,10 @@ public class LeaderPeerGroupActivity extends AppCompatActivity {
         }.start();
     }
 
-    private Boolean confirmToJoin(List<Peer> peerList){
-        return peerList.get(0).getLeader();
-    }
-    public void createPost(Retrofit retrofit) {
-        //  get the deserialized peer from RealTimeJourneyTableA
-        Peer peer = (Peer) getIntent().getSerializableExtra(RealTimeJourneyTableActivity.PEER_KEY);
+    public void createLeaderPost(Retrofit retrofit) {
         ReqResApi reqResApi = retrofit.create(ReqResApi.class);
+        Peer peer = (Peer) getIntent().getSerializableExtra(RealTimeJourneyTableActivity.PEER_KEY);
+        System.out.println("****************" + peer.toString());
         try {
             reqResApi.matchLeader(peer).enqueue(new Callback<List<Peer>>() {
                 @RequiresApi(api = Build.VERSION_CODES.O)
@@ -133,13 +133,12 @@ public class LeaderPeerGroupActivity extends AppCompatActivity {
                     List<Peer> peerList = response.body();
                     LeaderPeerAdapter leaderPeerAdapter = new LeaderPeerAdapter(LeaderPeerGroupActivity.this, peerList);
                     recyclerView.setAdapter(leaderPeerAdapter);
-                    sendPeersToNavigation(peerList);
-
+                    returnPeers = peerList;
                 }
 
                 @Override
                 public void onFailure(Call<List<Peer>> call, Throwable t) {
-                    System.out.println("-------------------on-failure--------------" + t.toString());
+                    System.out.println("-------------------on-failure1--------------" + t.fillInStackTrace());
                     Toast.makeText(LeaderPeerGroupActivity.this, t.toString(), Toast.LENGTH_SHORT).show();
                 }
             });
@@ -149,61 +148,21 @@ public class LeaderPeerGroupActivity extends AppCompatActivity {
         }
     }
 
+
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void sendPeersToNavigation(List<Peer> peerList) {
         confirm.setOnClickListener(view -> {
-            List<Peer> peers = peerList;
-            int peerIndex = 0;
-            if(!peers.isEmpty()){
+            if(peerList != null&&!peerList.isEmpty()){
                 //check which peer in peer list is current user
-                for (Peer peer : peerList) {
-                    peerIndex++;
-                    if(DialogueHelper.getSender().getEmail().equals(peer.getEmail())){
-                        //do not display the current user
-//                        peerList.remove(peerIndex-1);
-
-                        //send peer to navigation page
-                        Intent intent = new Intent(LeaderPeerGroupActivity.this, NavigationActivity.class);
-                        intent.putExtra(getString(R.string.PEER_LIST), FirebaseOperation.getObjectString(peerList));
-                        intent.putExtra(getString(R.string.CURRENT_PEER_EMAIL), peer.getEmail());
-                        //startActivity(intent);
-                        startActivityForResult(intent,1);
-                    }
-                }
+                Intent intent = new Intent(LeaderPeerGroupActivity.this, NavigationActivity.class);
+                intent.putExtra(getString(R.string.PEER_LIST), FirebaseOperation.getObjectString(peerList));
+                intent.putExtra(getString(R.string.CURRENT_PEER_EMAIL), DialogueHelper.getSender().getEmail());
             }else {
                 Toast.makeText(LeaderPeerGroupActivity.this, "There is no peer", Toast.LENGTH_SHORT).show();
             }
         });
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.O)
-    private void savePeerListToFile(List<Peer> peerList) throws IOException {
-        List<Peer> peers = null;
-        try {
-            ByteArrayOutputStream bos = new ByteArrayOutputStream();
-            ObjectOutputStream out = null;
-            out = new ObjectOutputStream(bos);
-            out.writeObject(peerList);
-            out.flush();
-            byte[] yourBytes = bos.toByteArray();
-            String str = Base64.getEncoder().encodeToString(yourBytes);
-            byte[] decodedBytes = Base64.getDecoder().decode(str);
-
-            ByteArrayInputStream bis = new ByteArrayInputStream(yourBytes);
-            ObjectInput in = null;
-            in = new ObjectInputStream(bis);
-            peers = (List<Peer>) in.readObject();
-
-            Toast.makeText(getBaseContext(), "save data successfully", Toast.LENGTH_LONG).show();
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (ClassNotFoundException e) {
-            e.printStackTrace();
-        }
-
-    }
 
     private void cancelJourney() {
         cancel.setOnClickListener(view -> {
@@ -211,30 +170,4 @@ public class LeaderPeerGroupActivity extends AppCompatActivity {
             startActivity(intent);
         });
     }
-
-
-//    @SuppressLint("NewApi")
-//    @RequiresApi(api = Build.VERSION_CODES.N)
-//    public String saveInfoToFirebase(List<Peer> peerList) {
-//        //to do
-//        Map<String, Object> record = new HashMap<>();
-//        Peer peer = peerList.stream().filter(o -> o.getLeader() == true).findAny().orElse(null);
-//        List<String> collect = peerList.stream().map(Peer::getEmail).collect(Collectors.toList());
-//        record.put("BelongTo", peer.getEmail());
-//        record.put("date", new Date());
-//        record.put("companion", collect);
-//
-//        String uuid = UUID.randomUUID().toString();
-//
-//        db.collection("record").document(uuid).set(record);
-//        for (Peer peer1 : peerList) {
-//            Map<String, Object> userRecord = new HashMap<>();
-//            userRecord.put("record_id", uuid);
-//            userRecord.put("email", peer1.getEmail());
-//            String uuid2 = UUID.randomUUID().toString();
-//            db.collection("user_record").document(uuid2).set(record);
-//        }
-//        return uuid;
-//    }
-
 }
