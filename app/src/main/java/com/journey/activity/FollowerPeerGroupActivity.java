ba229b56ea5e8cb7d4e3c55d2aeffb6e17900556
congqin yan <1727053412@qq.com>
2022-04-02 21:06:25 +0100

    update join and create

--- app/src/main/java/com/journey/activity/FollowerPeerGroupActivity.java
+++ app/src/main/java/com/journey/activity/FollowerPeerGroupActivity.java
@@ -21,6 +21,7 @@ import com.journey.adapter.ReqResApi;
 import com.journey.map.network.FirebaseOperation;
 import com.journey.model.Peer;
 import com.journey.service.database.DialogueHelper;
+import com.mapbox.bindgen.ArrayUtils;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
@@ -29,6 +30,7 @@ import java.io.IOException;
 import java.io.ObjectInput;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.util.Base64;
 import java.util.List;
 
@@ -51,6 +53,8 @@ public class FollowerPeerGroupActivity extends AppCompatActivity {
     Button follower_cancel;
     Button follower_confirm;
     CountDownTimer countDownTimer;
+
+    List<Peer> returnPeers;
     private static FirebaseFirestore db = FirebaseFirestore.getInstance();
     final private static String MATCHED_PEERS = "MATCHED_PEERS";
     LoadingDialog loadingDialog = new LoadingDialog(this,  8000, 2000, "");
@@ -78,6 +82,7 @@ public class FollowerPeerGroupActivity extends AppCompatActivity {
         try {
             loadingDialog.startLoadingDialog();
             sendMultiRequests(8000, 2000);
+            sendPeersToNavigation(returnPeers);
             cancelJourney();
 
         } catch (Exception e) {
@@ -87,10 +92,12 @@ public class FollowerPeerGroupActivity extends AppCompatActivity {
     }
 
     private void sendMultiRequests(Integer totalTime, Integer interval) {
+
+        Peer peer = (Peer) getIntent().getSerializableExtra(RealTimeJourneyTableActivity.PEER_KEY);
         new CountDownTimer(totalTime, interval) {
             @Override
             public void onTick(long millisUntilFinished) {
-                joinPost(retrofit);
+                joinPost(retrofit, peer);
             }
             @Override
             public void onFinish() {
@@ -99,9 +106,8 @@ public class FollowerPeerGroupActivity extends AppCompatActivity {
         }.start();
     }
 
-    public void joinPost(Retrofit retrofit) {
+    public void joinPost(Retrofit retrofit, Peer peer) {
         //  get the deserialized peer from RealTimeJourneyTableA
-        Peer peer = (Peer) getIntent().getSerializableExtra(RealTimeJourneyTableActivity.PEER_KEY);
         ReqResApi reqResApi = retrofit.create(ReqResApi.class);
         try {
             reqResApi.matchMemeber(peer).enqueue(new Callback<List<Peer>>() {
@@ -110,9 +116,9 @@ public class FollowerPeerGroupActivity extends AppCompatActivity {
                 public void onResponse(Call<List<Peer>> call, Response<List<Peer>> response) {
                     Toast.makeText(FollowerPeerGroupActivity.this, response.code() + " Response", Toast.LENGTH_SHORT).show();
                     List<Peer> peerList = response.body();
-                    LeaderPeerAdapter leaderPeerAdapter = new LeaderPeerAdapter(FollowerPeerGroupActivity.this, peerList);
-                    recyclerView.setAdapter(leaderPeerAdapter);
-                    sendPeersToNavigation(peerList);
+                    FollowerPeerAdapter followerPeerAdapter = new FollowerPeerAdapter(FollowerPeerGroupActivity.this, peerList);
+                    recyclerView.setAdapter(followerPeerAdapter);
+                    returnPeers = peerList;
                 }
 
                 @Override
@@ -130,24 +136,11 @@ public class FollowerPeerGroupActivity extends AppCompatActivity {
     @RequiresApi(api = Build.VERSION_CODES.O)
     private void sendPeersToNavigation(List<Peer> peerList) {
         follower_confirm.setOnClickListener(view -> {
-            List<Peer> peers = peerList;
-            int peerIndex = 0;
-            if(!peers.isEmpty()){
+            if(peerList != null&&!peerList.isEmpty()){
                 //check which peer in peer list is current user
-                for (Peer peer : peerList) {
-                    peerIndex++;
-                    if(DialogueHelper.getSender().getEmail().equals(peer.getEmail())){
-                        //do not display the current user
-//                        peerList.remove(peerIndex-1);
-
-                        //send peer to navigation page
-                        Intent intent = new Intent(FollowerPeerGroupActivity.this, NavigationActivity.class);
-                        intent.putExtra(getString(R.string.PEER_LIST), FirebaseOperation.getObjectString(peerList));
-                        intent.putExtra(getString(R.string.CURRENT_PEER_EMAIL), peer.getEmail());
-                        //startActivity(intent);
-                        startActivityForResult(intent,1);
-                    }
-                }
+                Intent intent = new Intent(FollowerPeerGroupActivity.this, NavigationActivity.class);
+                intent.putExtra(getString(R.string.PEER_LIST), FirebaseOperation.getObjectString(peerList));
+                intent.putExtra(getString(R.string.CURRENT_PEER_EMAIL), DialogueHelper.getSender().getEmail());
             }else {
                 Toast.makeText(FollowerPeerGroupActivity.this, "There is no peer", Toast.LENGTH_SHORT).show();
             }
